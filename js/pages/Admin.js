3a13705c323af075e31d0f58581eed673810b759
Akira <akiranshisthebestlol@gmail.com>
2026-08-12 07:13:38 +0800

    EMERGENCY: restore working Admin + List (site was broken by incomplete loaders)

--- js/pages/Admin.js
+++ js/pages/Admin.js
@@ -1,7 +1,1 @@
-const parts = await Promise.all([0,1,2,3,4,5].map(i => fetch(new URL("./Ab_p"+i+".txt", import.meta.url)).then(r => { if (!r.ok) throw new Error("Ab_p"+i); return r.text(); })));
-let code = atob(parts.join(""));
-const jsBase = new URL("../", import.meta.url).href;
-const pagesBase = new URL("./", import.meta.url).href;
-code = code.replace(/(from\s+['"])\.\.\/([^'"]+)(['"])/g, function (_, a, p, c) { return a + jsBase + p + c; });
-code = code.replace(/(from\s+['"])\.\/([^'"]+)(['"])/g, function (_, a, p, c) { return a + pagesBase + p + c; });
-export default (await import(URL.createObjectURL(new Blob([code], { type: "text/javascript" })))).default;
+export { default } from 'https://cdn.jsdelivr.net/gh/ziyyhhk/broken-team-demonlist@1562ed3c8171bf007a22daa608c535e9eef4f659/js/pages/Admin.js';
--- js/pages/List.js
+++ js/pages/List.js
@@ -1,7 +1,1 @@
-const parts = await Promise.all([0,1,2,3,4].map(i => fetch(new URL("./Lb_p"+i+".txt", import.meta.url)).then(r => { if (!r.ok) throw new Error("Lb_p"+i); return r.text(); })));
-let code = atob(parts.join(""));
-const jsBase = new URL("../", import.meta.url).href;
-const pagesBase = new URL("./", import.meta.url).href;
-code = code.replace(/(from\s+['"])\.\.\/([^'"]+)(['"])/g, function (_, a, p, c) { return a + jsBase + p + c; });
-code = code.replace(/(from\s+['"])\.\/([^'"]+)(['"])/g, function (_, a, p, c) { return a + pagesBase + p + c; });
-export default (await import(URL.createObjectURL(new Blob([code], { type: "text/javascript" })))).default;
+export { default } from 'https://cdn.jsdelivr.net/gh/ziyyhhk/broken-team-demonlist@00c0b84e795b090160ec033468a460887755e292/js/pages/List.js';
