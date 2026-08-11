0dcecf95f665ee9ef49adb8bcc9575a7fef751bb
Akira <akiranshisthebestlol@gmail.com>
2026-08-12 07:11:29 +0800

    List + Admin blob loaders (absolute import rewrite)

--- js/pages/Admin.js
+++ js/pages/Admin.js
@@ -1,1 +1,7 @@
-export { default } from 'https://cdn.jsdelivr.net/gh/ziyyhhk/broken-team-demonlist@1562ed3c8171bf007a22daa608c535e9eef4f659/js/pages/Admin.js';
+const parts = await Promise.all([0,1].map(i => fetch(new URL("./Admin_src_"+i+".txt", import.meta.url)).then(r => { if(!r.ok) throw new Error("Admin_src_"+i); return r.text(); })));
+let code = parts.join("");
+const jsBase = new URL("../", import.meta.url).href;
+const pagesBase = new URL("./", import.meta.url).href;
+code = code.replace(/(from\s+['"])\.\.\/([^'"]+)(['"])/g, function (_, a, p, c) { return a + jsBase + p + c; });
+code = code.replace(/(from\s+['"])\.\/([^'"]+)(['"])/g, function (_, a, p, c) { return a + pagesBase + p + c; });
+export default (await import(URL.createObjectURL(new Blob([code], { type: "text/javascript" })))).default;
